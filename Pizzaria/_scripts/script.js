093ebfd2ba0b28f1024104fe74d18f5a39ee0f7f
Bruniele <brunieledias123@gmail.com>
2023-07-30 12:07:56 -0300

    Adiciona animações na seção Domingo Perfeito

--- Pizzaria/_scripts/script.js
+++ Pizzaria/_scripts/script.js
@@ -72,3 +72,27 @@ $('ion-icon#youtube').click(function() {
 $('ion-icon#linkedin').click(function() {
   window.open("http://www.linkedin.com","_blank");
 })
+
+/* Códigos de Animação */
+
+$('section#domingo-perfeito').waypoint( function(direcao) {
+  if (direcao == "down") {
+    $('div.folha1').addClass('animate__animated animate__fadeInUp');
+    $('div.folha3').addClass('animate__animated animate__fadeInUp');
+    $('div.camarao').addClass('animate__animated animate__fadeInUp animate__slow');
+    $('div.tomate').addClass('animate__animated animate__fadeInUp animate__slow');
+    $('div.folha2').addClass('animate__animated animate__fadeInUp animate__slower');
+    $('div.queijo').addClass('animate__animated animate__fadeInUp animate__slower');
+  }
+
+  if (direcao == "up") {
+    $('div.folha1').removeClass('animate__animated animate__fadeInUp');
+    $('div.folha3').removeClass('animate__animated animate__fadeInUp');
+    $('div.camarao').removeClass('animate__animated animate__fadeInUp animate__slow');
+    $('div.tomate').removeClass('animate__animated animate__fadeInUp animate__slow');
+    $('div.folha2').removeClass('animate__animated animate__fadeInUp animate__slower');
+    $('div.queijo').removeClass('animate__animated animate__fadeInUp animate__slower'); 
+  }
+},{
+    offset: '450px;'
+})
